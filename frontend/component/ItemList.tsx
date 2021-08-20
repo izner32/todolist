aac0cf268511c6c024b138999169f509c650a3dd
Renz Carillo <renzcarillo32@gmail.com>
2021-08-20 20:43:46 +0800

    display items is finally working

--- frontend/component/ItemList.tsx
+++ frontend/component/ItemList.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 
-function ItemList({buttonClicked,setButtonClicked}) {
+export default function ItemList({buttonClicked,setButtonClicked}) {
 
     // store grabbed data from the get api into this variable
     const [itemList,setItemList] = useState([]);
@@ -8,9 +8,9 @@ function ItemList({buttonClicked,setButtonClicked}) {
     // fetch delete api request that lets us delete a data to the database
     const onRemoveButtonClicked = async () => {
 
-    // fetch the delete api
-    const res = await fetch(`http://localhost:4000/api/item-list`);
-    const data = await res.json(); // grab the response of the api
+        // fetch the delete api
+        const res = await fetch(`http://localhost:4000/api/item-list`);
+        const data = await res.json(); // grab the response of the api
 
     // change the value of state so useEffect would work thus updating the displayList
     setButtonClicked(!buttonClicked);
@@ -20,12 +20,13 @@ function ItemList({buttonClicked,setButtonClicked}) {
         
         // fetch the api that returns the list items array
         const fetchData = async () => {
-        const response = await fetch(`http://localhost:4000/api/item-list`)
-        const data = await response.json();
+            const response = await fetch(`http://localhost:4000/api/item-list`)
+            var data = await response.json();
 
-        // after fetching store this grabbed data now
-        setItemList(data);
+            // after fetching store this grabbed data now
+            setItemList(data);
         }
+        fetchData();
     }, [buttonClicked]);
 
     // check if array has no content - if no content return this
@@ -37,17 +38,22 @@ function ItemList({buttonClicked,setButtonClicked}) {
         )
     }
 
+    // console.log(itemList.map( (x) => x+5 ));
     // now that you have reached this level it means the array has content
     // map the array and return an array containing each of these jsx elements
-    itemList.map( (x,key) => (
-        <div className="bg-light d-flex justify-content-between align-items-center mb-3" key={key}>
-            <li className="ms-2">{x}</li>
-            <button 
-                className ="btn btn-dark"
-                onClick = {() => { onRemoveButtonClicked() }}
-            >remove</button>
-        </div>
-    ) );
+    return (
+        <> {/* when returning a map, make sure to wrap it in a react fragment, fck this small bugs */}
+            {
+            itemList.map( (x,key) => ( //* make sure to also contain the map inside the return, fck this small bugs 
+            <div className="bg-light d-flex justify-content-between align-items-center mb-3" key={key}>
+                <li className="ms-2">{x}</li>
+                <button 
+                    className ="btn btn-dark"
+                    onClick = {() => { onRemoveButtonClicked() }}
+                >remove</button>
+            </div>
+            ) )
+            }
+        </>
+    )
 }
-
-export default ItemList
